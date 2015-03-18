83479c1ca6c81b4e56b0b55e16f7fb3b6c8b896e
Kyle Ames <KyleAmes13@gmail.com>
2015-03-18 10:08:21 -0400

    Added scripts directory. Updated setup.sh.

--- setup.sh
+++ setup.sh
@@ -4,15 +4,22 @@
 ln -s ~/.dot_files/bash/bashrc .bashrc
 ln -s ~/.dot_files/vimrc .vimrc
 ln -s ~/.dot_files/tmux.conf .tmux.conf
-if [[ $( uname ) == 'Darwin' ]]; then
+if [ $( uname ) == 'Darwin' ]; then
     ln -s ~/.dot_files/osx/profile .profile
 fi
 
-# Source everything for the next steps
-source ~/.bashrc
-
-# If we haven't set up vundle go ahead and do so
-if [[ ! -f ~/.vim/bundle/Vundle.vim ]]; then
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
+# Get fzf if we haven't yet
+if [ ! -d ~/.fzf ]; then
+  gitt clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
 fi
+
+# Get vim-plug if we haven't yet
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim +PlugInstall +qall
+fi
+
+# Source the bashrc we just pulled down
+source ~/.bashrc
